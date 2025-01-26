ddfcb95e5858a235195c518dabc6981cc3e16f06
dmitryger <you@example.com>
2025-01-26 13:21:28 +0300

    настройка ci-cd

--- tests/test_tenders.py
+++ tests/test_tenders.py
@@ -1,8 +1,11 @@
 import pytest
+import testit
 from pages.tenders import TendersPage
 
 @pytest.mark.smoke
 class TestTendersPage:
+    @testit.workItemID(43)
+    @testit.displayName('test_input_notice')
     def test_input_notice(self, browser):
         tender = TendersPage(browser)
         tender.check_input_notice()
