8dc49b0d8a04e615b202a93d1867fb132f11315f
dmitryger <you@example.com>
2025-01-26 14:49:07 +0300

    настройка ci-cd

--- fixtures/page.py
+++ fixtures/page.py
@@ -7,7 +7,7 @@ import os
 def pytest_addoption(parser):
     """Пользовательские опции командной строки"""
     parser.addoption('--bn', action='store', default="remote_chrome", help="Choose browser: chrome, remote_chrome or firefox")
-    parser.addoption('--h', action='store', default=True, help='Choose headless: True or False')
+    parser.addoption('--h', action='store', default=False, help='Choose headless: True or False')
     parser.addoption('--s', action='store', default={'width': 1920, 'height': 1080}, help='Size window: width,height')
     parser.addoption('--slow', action='store', default=200, help='Choose slow_mo for robot action')
     parser.addoption('--t', action='store', default=60000, help='Choose timeout')
